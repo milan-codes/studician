b37a576dd9f7813fc816bc01b1cf45fce79e4915
milan-codes <milanherke@protonmail.com>
2023-11-25 12:32:11 +0100

    feat(api): added custom password validator

--- apps/api/utils/validation.go
+++ apps/api/utils/validation.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"net/http"
+	"regexp"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
@@ -12,8 +13,23 @@ type CustomValidator struct {
 }
 
 func (cv *CustomValidator) Validate(i interface{}) error {
+	cv.Validator.RegisterValidation("pwd", validatePassword)
   if err := cv.Validator.Struct(i); err != nil {
     return echo.NewHTTPError(http.StatusBadRequest, err.Error())
   }
   return nil
+}
+
+func validatePassword(fl validator.FieldLevel) bool {
+  password := fl.Field().String()
+  numeric := regexp.MustCompile(`[0-9]`)
+  uppercase := regexp.MustCompile(`[A-Z]`)
+  lowercase := regexp.MustCompile(`[a-z]`)
+  special := regexp.MustCompile(`[!@#$%^&*()_+]`)
+  return len(password) >= 8 &&
+  		 len(password) <= 64 &&
+		 numeric.MatchString(password) &&
+		 uppercase.MatchString(password) &&
+		 lowercase.MatchString(password) &&
+		 special.MatchString(password)
 }
