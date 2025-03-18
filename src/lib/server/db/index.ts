28caf31c6c3a1025ada68c3539f8ae16542cd9d1
milan-codes <milanherke@protonmail.com>
2025-03-18 09:00:41 +0100

    feat: enable database logger in development

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
+
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 const client = postgres(env.DATABASE_URL);
-export const db = drizzle(client, { casing: 'snake_case' });
+export const db = drizzle(client, { casing: 'snake_case', logger: dev });
