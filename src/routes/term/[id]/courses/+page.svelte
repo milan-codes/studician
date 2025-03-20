540204c0241655ae17e7928e20f8f0de5a02c86a
milan-codes <milanherke@protonmail.com>
2025-03-20 11:51:30 +0100

    feat: list courses

--- src/routes/term/[id]/courses/+page.svelte
+++ src/routes/term/[id]/courses/+page.svelte
@@ -3,17 +3,25 @@
 	import { Separator } from '$lib/components/ui/separator';
 	import { Plus } from 'lucide-svelte';
 	import type { PageProps } from './$types';
+	import CourseCard from './course-card.svelte';
+	import { cn } from '$lib/utils';
 
 	let { data }: PageProps = $props();
 </script>
 
-<div class="flex items-center justify-between">
+<div class="flex flex-col items-center justify-between gap-4 md:flex-row">
 	<div class="space-y-0.5">
 		<h2 class="text-2xl font-bold tracking-tight">Courses</h2>
 		<p class="text-muted-foreground">You can find all the courses you have for this term here</p>
 	</div>
-	<a href={`/term/${data.activeTerm.id}/courses/new`} class={buttonVariants()}
-		><Plus /> Create new course</a
+	<a
+		href={`/term/${data.activeTerm.id}/courses/new`}
+		class={cn('w-full md:w-auto', buttonVariants())}><Plus /> Create new course</a
 	>
 </div>
 <Separator class="my-8" />
+<div class="grid gap-4">
+	{#each data.courses as course}
+		<CourseCard {course} />
+	{/each}
+</div>
---
+++ src/routes/term/[id]/courses/course-card.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.js';
+	import type { Course } from '$lib/server/db/schemas/course';
+	import { formatDate } from '$lib/utils';
+	import { Circle } from 'lucide-svelte';
+
+	let { course }: { course: Course } = $props();
+</script>
+
+<a href={`/term/${course.termId}/courses/${course.id}`}>
+	<Card.Root class="transition-all hover:bg-muted">
+		<Card.Header class="flex flex-row items-center justify-between gap-4 space-y-0">
+			<div class="space-y-2">
+				<Card.Title>{course.name}</Card.Title>
+				<Card.Description>
+					{course.description ?? 'No description provided'}
+				</Card.Description>
+			</div>
+		</Card.Header>
+		<Card.Content>
+			<div class="flex gap-4 text-sm text-muted-foreground">
+				<div class="flex items-center">
+					<Circle class="mr-1 h-4 w-4" fill={course.color} color={course.color} />
+				</div>
+				Updated {formatDate(course.updatedAt)}
+			</div>
+		</Card.Content>
+	</Card.Root>
+</a>
