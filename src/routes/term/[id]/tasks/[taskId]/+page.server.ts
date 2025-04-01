9d4b48cbd5f8edccf7cfc0d3c0a8661dfe7c8277
milan-codes <milanherke@protonmail.com>
2025-04-01 16:38:58 +0200

    feat: edit tasks

---
+++ src/routes/term/[id]/tasks/[taskId]/+page.server.ts
@@ -0,0 +1,63 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { db } from '$lib/server/db';
+import { course as courseTable } from '$lib/server/db/schema';
+import { and, eq, getTableColumns } from 'drizzle-orm';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { formSchema } from './schema';
+import type { Actions } from './$types';
+import { message } from 'sveltekit-superforms';
+import { task as taskTable } from '$lib/server/db/schema';
+import { term as termTable } from '$lib/server/db/schema';
+
+export const load: PageServerLoad = async (event) => {
+    if (!event.locals.user || !event.locals.profile) return redirect(302, '/login');
+
+    const taskWhere = and(eq(taskTable.id, event.params.taskId), eq(courseTable.termId, event.params.id));
+    const [task] = await db
+        .select({ ...getTableColumns(taskTable) })
+        .from(taskTable)
+        .innerJoin(courseTable, eq(courseTable.id, taskTable.courseId))
+        .where(taskWhere)
+        .limit(1);
+
+    if (!task) return redirect(302, `/term/${event.params.id}/tasks`);
+
+        const courseWhere = and(eq(termTable.userId, event.locals.user.id));
+    
+        const courses = await db
+            .select({ ...getTableColumns(courseTable) })
+            .from(courseTable)
+            .innerJoin(termTable, eq(termTable.id, courseTable.termId))
+            .where(courseWhere);
+
+    const taskWithoutNullValues = {
+        ...task,
+        description: task.description ?? undefined
+    };
+
+    return { form: await superValidate(taskWithoutNullValues, zod(formSchema)), courses };
+};
+
+export const actions: Actions = {
+    default: async (event) => {
+        const userId = event.locals.user?.id;
+        if (!userId) return redirect(302, '/login');
+
+        const form = await superValidate(event, zod(formSchema));
+        if (!form.valid) return message(form, 'Invalid form');
+
+        const { id: termId, taskId } = event.params;
+
+        const parsedTask = {
+            ...form.data,
+            description: form.data.description ? form.data.description : null
+        };
+
+        const where = and(eq(taskTable.id, taskId));
+        await db.update(taskTable).set(parsedTask).where(where);
+
+        return redirect(302, `/term/${termId}/tasks`);
+    }
+};
---
+++ src/routes/term/[id]/tasks/[taskId]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Separator } from '$lib/components/ui/separator';
+	import type { PageData } from './$types';
+	import EditTaskForm from './edit-task-form.svelte';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<div class="flex items-center justify-between">
+	<div class="space-y-0.5">
+		<h2 class="text-2xl font-bold tracking-tight">Edit task</h2>
+		<p class="text-muted-foreground">Edit the selected task</p>
+	</div>
+</div>
+<Separator class="my-4" />
+<EditTaskForm {data} />
