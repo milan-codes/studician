f2447810acafa4bdaf632a8709bb861a7d7dc6f9
milan-codes <milanherke@protonmail.com>
2025-04-13 14:36:23 +0200

    feat: edit activities

---
+++ src/routes/term/[id]/activities/[activityId]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Separator } from '$lib/components/ui/separator';
+	import type { PageData } from './$types';
+	import EditActivityForm from './edit-activity-form.svelte';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<svelte:head>
+	<title>Edit {data.form.data.name}</title>
+</svelte:head>
+
+<div class="flex items-center justify-between">
+	<div class="space-y-0.5">
+		<h2 class="text-2xl font-bold tracking-tight">Edit activity</h2>
+		<p class="text-muted-foreground">Edit the selected activity</p>
+	</div>
+</div>
+<Separator class="my-4" />
+<EditActivityForm {data} />
